c59a0278d0f0d90fe11680e84448eb3379dcd80a
Aleksander Heintz <alxandr@alxandr.me>
2020-05-31 19:40:30 +0200

    Feat/google analytics (#45)

---
+++ pages/_document.tsx
@@ -0,0 +1,40 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+const GaTag = ({ trackingId }: { trackingId: string | undefined }) => {
+  if (!trackingId) return null;
+
+  return (
+    <>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${trackingId}');
+          `,
+        }}
+      ></script>
+    </>
+  );
+};
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <GaTag trackingId={process.env.GA_TRACKING_ID} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
